575f4eb2ff7e76b6d722c58ddf19d272d427a83c
Pavel Koryakin <unclekoin@gmail.com>
2021-05-30 13:29:47 +0300

    feat: add some functionality

--- upload.js
+++ upload.js
@@ -1,12 +1,37 @@
+function formatBytes(bytes, decimals = 2) {
+  if (!bytes) return '0 Bytes';
+
+  const k = 1024;
+  const dm = decimals < 0 ? 0 : decimals;
+  const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+
+  const i = Math.floor(Math.log(bytes) / Math.log(k));
+
+  return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+}
+
+const element = (tag, classes = [], content = '') => {
+  const node = document.createElement(tag);
+
+  if (classes.length) {
+    node.classList.add(...classes);
+  }
+
+  if (content) {
+    node.textContent = content;
+  }
+
+  return node;
+}
+
+
 export function upload(selector, options = {}) {
+  let files = [];
   const input = document.querySelector(selector);
-  const preview = document.createElement('div');
-
-  preview.classList.add('preview');
-
-  const open = document.createElement('button');
-  open.classList.add('btn');
-  open.textContent = 'Select';
+  const preview = element('div', ['preview']);
+  const open = element('button', ['btn'], 'Select');
+  const upload = element('button', ['btn', 'primary'], 'Download');
+  upload.style.display = 'none';
 
   if (options.multi) {
     input.setAttribute('multiple', true)
@@ -17,6 +42,7 @@ export function upload(selector, options = {}) {
   }
 
   input.insertAdjacentElement('afterend', preview);
+  input.insertAdjacentElement('afterend', upload);
   input.insertAdjacentElement('afterend', open);
 
   const triggerInput = () => input.click();
@@ -26,18 +52,31 @@ export function upload(selector, options = {}) {
       return;
     }
 
-    const files = Array.from(event.target.files);
+    files = Array.from(event.target.files);
+    preview.innerHTML = '';
+    upload.style.display = 'inline-block';
 
     files.forEach((file) => {
       if (!/image/.test(file.type)) {
         return;
       }
 
+
+
       const reader = new FileReader();
 
       reader.onload = (e) => {
-        console.log(e.target.result);
-        input.insertAdjacentHTML('afterend', `<img src=${e.target.result} alt="" width="70">`)
+        const src = e.target.result;
+        preview.insertAdjacentHTML('afterbegin', `
+          <div class="preview__image">
+            <div class="preview__remove" data-name="${file.name}">&times;</div>
+            <img src="${ src }" alt="${ file.name }">
+            <div class="preview__info">
+                <span>${file.name}</span>
+                <span>${formatBytes(file.size)}</span>
+            </div>
+          </div>
+        `)
       }
 
       reader.readAsDataURL(file);
@@ -45,6 +84,32 @@ export function upload(selector, options = {}) {
 
   }
 
+  const removeHandler = (e) => {
+    if (!e.target.dataset.name) {
+      return;
+    }
+
+    const { name } = e.target.dataset;
+    files = files.filter((file) => file.name !== name);
+
+    if (!files.length) {
+      upload.style.display = 'none';
+    }
+
+    const block = preview
+      .querySelector(`[data-name="${ name }"]`)
+      .closest('.preview__image')
+
+    block.classList.add('removing');
+    setTimeout(() => block.remove(), 300);
+  }
+
+  const uploadHandler = () => {
+
+  }
+
   open.addEventListener('click', triggerInput);
-  input.addEventListener('change', changeHandler)
+  input.addEventListener('change', changeHandler);
+  preview.addEventListener('click', removeHandler);
+  upload.addEventListener('click', uploadHandler);
 }
